6ffca6e2abf73b5e64949c6d974681e6c5f5908f
Andrey Belyaev <learnscripmail@gmail.com>
2023-06-19 17:25:44 +0300

    add tw

--- src/screens/MainScreen/MainScreen.tsx
+++ src/screens/MainScreen/MainScreen.tsx
@@ -1,3 +1,3 @@
 export const MainScreen = () => {
-  return <div>Main Page</div>;
+  return <div className="text-[24px]">Main Page</div>;
 };
---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+const twColors = require('tailwindcss/colors')
+
+
+const colors = {
+	transparent: twColors.transparent,
+	black: '#2E3239',
+	gray: '#CDCDCD',
+	white: twColors.white,
+	primary: '#FF9902',
+	secondary: '#161D25',
+	'bg-color': '#F2F2F5',
+	aqua: '#268697',
+	red: twColors.red[400]
+}
+
+
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		colors,
+		extend: {
+			fontSize: {
+				sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+			},
+			keyframes: {
+				animationOpacity: {
+					from: { opacity: 0.2 },
+					to: { opacity: 1 }
+				},
+				scaleIn: {
+					'0%': {
+						opacity: 0,
+						transform: 'scale(0.9)'
+					},
+					'50%': {
+						opacity: 0.3
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'scale(1)'
+					}
+				}
+			},
+			animation: {
+				opacity: 'animationOpacity .5s ease-in-out',
+				scaleIn: 'scaleIn .35s ease-in-out'
+			}
+		}
+	},
+	plugins: []
+}
+
+
