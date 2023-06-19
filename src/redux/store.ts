432a546b12fe9a2ba5d8e2d4af38044682fbef06
Andrey Belyaev <learnscripmail@gmail.com>
2023-06-19 14:56:12 +0300

    start

---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// TODO: Импортируйте ваши срезы (slices) и редьюсеры
+
+const store = configureStore({
+  reducer: {
+    // TODO: Добавьте ваши срезы (slices) и редьюсеры здесь
+  },
+});
+
+export default store;
